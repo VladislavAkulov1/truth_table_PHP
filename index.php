908107fd56b9b68c9f575a02b44ff7f8f8f678b8
VladislavAkulov1 <vlad_akylov.03@icloud.com>
2023-04-10 20:34:45 +0200

    first commit

---
+++ index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+    <title>Таблица истинности</title>
+    <link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+    <h1>Таблица истинности
+    <table>
+        <thead>
+            <tr>
+                <th>A</th>
+                <th>B</th>
+                <th>!A</th>
+                <th>A || B</th>
+                <th>A && B</th>
+                <th>A xor B</th>
+            </tr>
+        </thead>
+        <tbody>
+            <?php include 'table.php'; ?>
+        </tbody>
+    </table></h1>
+</body>
+</html>
